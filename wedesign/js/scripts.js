bc92506e34fa248bfc4bf7bd855acc13b0e2ef90
José Reato Neto <jreato@ciandt.com>
2019-08-21 22:20:27 -0300

    conclusão projeto 1

--- wedesign/js/scripts.js
+++ wedesign/js/scripts.js
@@ -1,8 +1,13 @@
 
 // Menu Mobile
 
+document.getElementById("hamburguer-icon").onclick =  function() {
+    document.getElementById("sliding-header-menu-outer").style['right'] = "0px";   
+};
 
-
+document.getElementById("btnCloseMenu").onclick =  function() {
+  document.getElementById("sliding-header-menu-outer").style['right'] = "-350px";   
+};
 
 // About us Tab
 
@@ -15,7 +20,25 @@ var aboutUs = {
 var unseletected_color = "#646872";
 var seletected_color = "#2A2D34";
 
+var tabs = document.getElementsByClassName("single-tab");
+var box = document.getElementById("box-text");
+box.innerHTML = aboutUs.Missão;
 
+for (var i = 0; i < tabs.length; i++) {   
+
+    tabs[i].onclick = function () {
+
+        for (var j = 0; j < tabs.length; j++) {
+            tabs[j].style['background-color'] = unseletected_color;
+            tabs[j].style['font-weight'] = "normal"; 
+        }
+
+        this.style['background-color'] = seletected_color;
+        this.style['font-weight'] = "bold";  
+        
+        box.innerHTML = aboutUs[this.innerHTML];
+    };
+}
 
 // Slider de serviços
 
@@ -37,12 +60,43 @@ var our_services = [
   
 ];
 
+var servico_atual = 0;
+var title = document.getElementById("service-title");
+var text = document.getElementById("service-text");
+
+title.innerHTML = our_services[servico_atual].title;
+text.innerHTML = our_services[servico_atual].text;
+
+document.getElementById("service-next").onclick = function () {
+  if (servico_atual < our_services.length - 1) { 
+    servico_atual++;
+  } else  {
+    servico_atual = 0;
+  }
+  title.innerHTML = our_services[servico_atual].title;
+  text.innerHTML = our_services[servico_atual].text;
+};
+
+document.getElementById("service-previous").onclick = function () {
+  if (servico_atual > 0) {
+    servico_atual--;
+  } else {
+    servico_atual = our_services.length - 1;
+  }
+
+    title.innerHTML = our_services[servico_atual].title;
+    text.innerHTML = our_services[servico_atual].text;
+  
+};
 
 // Data Footer
 
+var ano_atual = new Date();
+ano_atual = ano_atual.getFullYear();
+document.getElementById("current_year").innerHTML = ano_atual;
 
+//Api Key google maps: AIzaSyBIV27s9DvzUMSZsxFxpLhIrq0g7z1D29o
 
-  
    
 
 
