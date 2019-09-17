2b3baae70a0228018496fca50d45aa864fa5ed7e
José Reato Neto <jreato@ciandt.com>
2019-09-16 22:13:29 -0300

    antes do projeto 2 camisetas

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -1,170 +1,219 @@
-//var caixa_azul = document.getElementById("caixa_azul").innerHTML;
-//document.getElementById("caixa_azul").innerHTML = '<h1>' + caixa_azul + '</h1>';
+$(document).ready(function(){
 
-//Aula 11
-//function valor_imc(peso, altura) {
-//    var imc = peso / (altura * altura);
-//    return imc;
-//}
 
-//var peso = parseFloat(document.getElementById("peso").innerHTML);
-//console.log(typeof peso);
-//var altura = parseFloat(document.getElementById("altura").innerHTML)
-//var imc = valor_imc(peso, altura);
-//document.getElementById("imc").innerHTML = imc.toFixed(2);
+    //var caixa_azul = document.getElementById("caixa_azul").innerHTML;
+    //document.getElementById("caixa_azul").innerHTML = '<h1>' + caixa_azul + '</h1>';
 
-//Aula 14
-// var cursos = [
-//     {
-//         'titulo': 'Aprenda programação em Python 3 com facilidade do zero',
-//         'avaliacoes': 680,
-//         'alunos': 2300,
-//         'categorias': ['programacao', 'tecnologia']
-//     },
+    //Aula 11
+    //function valor_imc(peso, altura) {
+    //    var imc = peso / (altura * altura);
+    //    return imc;
+    //}
 
-//     {
-//         'titulo': 'Aprenda PHP e faça sites dinâmicos',
-//         'avaliacoes': 180,
-//         'alunos': 350,
-//         'categorias': ['desenvolvimento web', 'programacao']
-//     },
+    //var peso = parseFloat(document.getElementById("peso").innerHTML);
+    //console.log(typeof peso);
+    //var altura = parseFloat(document.getElementById("altura").innerHTML)
+    //var imc = valor_imc(peso, altura);
+    //document.getElementById("imc").innerHTML = imc.toFixed(2);
 
-//     {
-//         'titulo': 'Excel do Zero ao Avançado',
-//         'avaliacoes': 420,
-//         'alunos': 1800,
-//         'categorias': ['produtividade', 'gestão']
-//     }
-    
-// ];
-            
-// console.log(cursos[1].categorias[0]);
+    //Aula 14
+    // var cursos = [
+    //     {
+    //         'titulo': 'Aprenda programação em Python 3 com facilidade do zero',
+    //         'avaliacoes': 680,
+    //         'alunos': 2300,
+    //         'categorias': ['programacao', 'tecnologia']
+    //     },
 
-// Aula 15
-var aluno = {
-    'nome': 'Maria',
-    'sobrenome': 'Pereira',
-    'ano_nasc': 1984,
-    'nome_completo': function() {
-        var nomeCompleto = this.nome + ' ' + this.sobrenome;
-        return nomeCompleto;
-    },
-    'idade_aluno': function() {
-        return 2019 - this.ano_nasc;
-    } 
-}
+    //     {
+    //         'titulo': 'Aprenda PHP e faça sites dinâmicos',
+    //         'avaliacoes': 180,
+    //         'alunos': 350,
+    //         'categorias': ['desenvolvimento web', 'programacao']
+    //     },
 
-console.log(aluno.nome_completo());
-console.log(aluno.idade_aluno());
+    //     {
+    //         'titulo': 'Excel do Zero ao Avançado',
+    //         'avaliacoes': 420,
+    //         'alunos': 1800,
+    //         'categorias': ['produtividade', 'gestão']
+    //     }
+        
+    // ];
+                
+    // console.log(cursos[1].categorias[0]);
 
-//aula 16
-console.log("--Aula 16--");
+    // Aula 15
+    // var aluno = {
+    //     'nome': 'Maria',
+    //     'sobrenome': 'Pereira',
+    //     'ano_nasc': 1984,
+    //     'nome_completo': function() {
+    //         var nomeCompleto = this.nome + ' ' + this.sobrenome;
+    //         return nomeCompleto;
+    //     },
+    //     'idade_aluno': function() {
+    //         return 2019 - this.ano_nasc;
+    //     } 
+    // }
 
-// document.getElementById("click-me").onclick = function () {
-//     alert("Você clicou no botão");
-// };
+    // console.log(aluno.nome_completo());
+    // console.log(aluno.idade_aluno());
 
-// document.getElementById("hover-me").onmouseover = function () {
-//     alert("Você passou com o cursor no botão");
-// };
+    //aula 16
+    console.log("--Aula 16--");
 
-// document.getElementById("leave-me").onmouseleave = function () {
-//     alert("Você saiu com o cursor no botão");
-// };
+    // document.getElementById("click-me").onclick = function () {
+    //     alert("Você clicou no botão");
+    // };
 
-// document.onkeydown = function() {
-//     alert('Você apertou alguma tecla ' + event.keyCode);
-// };
+    // document.getElementById("hover-me").onmouseover = function () {
+    //     alert("Você passou com o cursor no botão");
+    // };
 
-//Aula 17
-console.log("--aula 17--");
-// document.getElementById("botao_cor").onclick =  function() {
-//     document.getElementById("botao_cor").style['background-color'] = "purple";
-//     document.getElementById("botao_cor").style.transform = "translateX(100px)";
-// };
-//forma 2
-// document.getElementById("botao_cor").onclick =  function() {
-//     this.style['background-color'] = "purple";
-//     this.style.transform = "translateX(100px)";
-// };
+    // document.getElementById("leave-me").onmouseleave = function () {
+    //     alert("Você saiu com o cursor no botão");
+    // };
 
-//forma 3
-// var botao = document.getElementById("botao_cor");
-// botao.onclick =  function() {
-//     botao.style['background-color'] = "purple";
-//     botao.style.transform = "translateX(100px)";
-// };
+    // document.onkeydown = function() {
+    //     alert('Você apertou alguma tecla ' + event.keyCode);
+    // };
 
-//Aula 18
-console.log("--Aula 18--");
+    //Aula 17
+    console.log("--aula 17--");
+    // document.getElementById("botao_cor").onclick =  function() {
+    //     document.getElementById("botao_cor").style['background-color'] = "purple";
+    //     document.getElementById("botao_cor").style.transform = "translateX(100px)";
+    // };
+    //forma 2
+    // document.getElementById("botao_cor").onclick =  function() {
+    //     this.style['background-color'] = "purple";
+    //     this.style.transform = "translateX(100px)";
+    // };
 
-// var elementos = document.getElementsByClassName("exemplo");
-// console.log(elementos);
-// elementos[0].innerHTML = "teste 1";
+    //forma 3
+    // var botao = document.getElementById("botao_cor");
+    // botao.onclick =  function() {
+    //     botao.style['background-color'] = "purple";
+    //     botao.style.transform = "translateX(100px)";
+    // };
 
-// var elementosP = document.getElementsByTagName("p");
-// console.log(elementosP);
+    //Aula 18
+    console.log("--Aula 18--");
 
-//Aula 19
-console.log("--Aula 19--");
+    // var elementos = document.getElementsByClassName("exemplo");
+    // console.log(elementos);
+    // elementos[0].innerHTML = "teste 1";
 
-//var elementos = document.getElementsByClassName("exemplo");
+    // var elementosP = document.getElementsByTagName("p");
+    // console.log(elementosP);
 
-// for (var i = 0; i < elementos.length; i++)
-// {
-//     elementos[i].style.color = "orange";
-//     elementos[i].style['font-weight'] = "bold";
-// }
+    //Aula 19
+    console.log("--Aula 19--");
 
-// for (var e in elementos)
-// {
-//     console.log(e);
-// }
-// var count = 0
-// while (count < elementos.length) {
-//     elementos[count].style.color = "blue";
-//     count++;
-// }
+    //var elementos = document.getElementsByClassName("exemplo");
 
-// console.log(count);
-// count = 2;
-// do {
-//     elementos[count].style.color = "red";
-//     count--;
-// } while (count != 0);
+    // for (var i = 0; i < elementos.length; i++)
+    // {
+    //     elementos[i].style.color = "orange";
+    //     elementos[i].style['font-weight'] = "bold";
+    // }
 
-//
-//Aula 19
- console.log("--Aula 27--");
-// console.log("Mensagem 1");
+    // for (var e in elementos)
+    // {
+    //     console.log(e);
+    // }
+    // var count = 0
+    // while (count < elementos.length) {
+    //     elementos[count].style.color = "blue";
+    //     count++;
+    // }
 
-// window.setTimeout(function() {
-//     console.log("Mensagem 2");
-// },3000);
+    // console.log(count);
+    // count = 2;
+    // do {
+    //     elementos[count].style.color = "red";
+    //     count--;
+    // } while (count != 0);
 
-// document.getElementById("mostrar-loader").onclick = function () {
+    //
+    //Aula 19
+    console.log("--Aula 27--");
+    // console.log("Mensagem 1");
 
-//     document.getElementById("spinner-loader").style.display = "initial";
-//     window.setTimeout(function(){
-//         document.getElementById("spinner-loader").style.display = "none";
-//     }, 5000);
+    // window.setTimeout(function() {
+    //     console.log("Mensagem 2");
+    // },3000);
 
-// }; 
+    // document.getElementById("mostrar-loader").onclick = function () {
 
-// var count = 0;
-//     window.setInterval(function(){
-//         console.log(count);
-//         count++;
-//     }, 1000);
+    //     document.getElementById("spinner-loader").style.display = "initial";
+    //     window.setTimeout(function(){
+    //         document.getElementById("spinner-loader").style.display = "none";
+    //     }, 5000);
 
-var relogio = document.getElementById("relogio");
-relogio.innerHTML = "";
+    // }; 
 
-window.setInterval(function() {
-    var data = new Date();
-    var h = data.getHours();
-    var m = data.getMinutes();
-    var s = data.getSeconds();
-    relogio.innerHTML = h + ':' + m + ':' + s;
-},1000); 
+    // var count = 0;
+    //     window.setInterval(function(){
+    //         console.log(count);
+    //         count++;
+    //     }, 1000);
 
+    // var relogio = document.getElementById("relogio");
+    // relogio.innerHTML = "";
+
+    // window.setInterval(function() {
+    //     var data = new Date();
+    //     var h = data.getHours();
+    //     var m = data.getMinutes();
+    //     var s = data.getSeconds();
+    //     relogio.innerHTML = h + ':' + m + ':' + s;
+    // },1000); 
+
+    console.log("--Aula 36--");
+
+    // var elementos = $("#interesses li");
+    // $.each(elementos, function(indice, valor){
+    //     console.log($(valor).text());
+    // });
+
+    console.log("--Aula 39--");
+
+    // $("#nome").on({ 
+    //     'keyup': function() {
+    //         if ($(this).val() == "") {
+    //             $("#confirmar").hide();
+    //         } else {
+    //             $("#confirmar").show();
+    //         }
+
+    //     }
+
+    // });
+
+    console.log("--Aula 42--");
+
+    // $("#animar").click(function(){
+    //     $("#quadrado").animate({
+    //         width: "+=100px"
+    //     }, 5000);
+
+    //     $("#quadrado").animate({
+    //         height: "+=100px"
+    //     }, 5000);
+    // });
+
+    console.log("--Aula 43--");
+    $("#animar").click(function(){
+        $("#quadrado").animate({
+            width: "+=100px"
+        }, 5000).animate({
+            height: "+=100px"
+        }, 5000, function() {
+            $("#quadrado").css("background-color","green");
+        });
+
+         
+    });
+});   
